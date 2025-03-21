40f9685a78253f5542d8c7a959786b440523495b
G Yashwanth Kumar <102477190+yashyash0110@users.noreply.github.com>
2025-03-21 15:28:34 +0530

    Create env.sv

---
+++ codes/verification/env.sv
@@ -0,0 +1,29 @@
+//Environment Class
+class env extends uvm_env;
+  `uvm_component_utils(env)
+  
+  scoreboard scd;
+  agent agnt;
+
+  config_dff cfg;
+  
+  function new(string name = "env", uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase); 
+    scd = scoreboard::type_id::create("scd",this);
+    agnt = agent::type_id::create("agnt",this);
+    cfg = config_dff::type_id::create("cfg",this);
+    
+    uvm_config_db#(config_dff)::set(this,"agnt","cfg",cfg);
+    
+  endfunction
+  
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    agnt.mon.mon_port.connect(scd.recv);
+  endfunction
+  
+endclass
